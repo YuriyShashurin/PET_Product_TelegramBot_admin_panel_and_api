d594884e6fc068a929651d92023bd0ee504862ed
Yury Shashurin <shashurin.edu@mail.ru>
2022-04-05 20:18:51 +0300

    add heroku files

---
+++ Procfile
@@ -0,0 +1,4 @@
+release: python manage.py migrate
+release: python manage.py createsuperuser
+release: python manage.py collectstatic
+web: python manage.py runserver 0.0.0.0:$PORT
--- admin_bot/settings.py
+++ admin_bot/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'admin_bot.urls'
@@ -80,16 +82,7 @@ WSGI_APPLICATION = 'admin_bot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'product_bot',
-        'USER': 'postgres',
-        'PASSWORD': 'as89lokan07',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 
 REST_FRAMEWORK = {
@@ -151,3 +144,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

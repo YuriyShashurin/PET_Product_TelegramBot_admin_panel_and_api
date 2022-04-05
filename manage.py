b5944ce72bc013a8aa3e546cbfa6f961210c370e
Yury Shashurin <shashurin.edu@mail.ru>
2022-04-05 22:42:43 +0300

    delete env

--- manage.py
+++ manage.py
@@ -7,9 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_bot.settings')
-    os.environ.setdefault('DB_LOCAL', 'True')
-    os.environ.setdefault('TOKEN_ADMIN_BOT', 'admin_bot.django-insecure-jaes6cu-vorpqsppr4c#(3i87x81mc(up2f(h7axwkgs50%mg(')
-    os.environ.setdefault('DB_PSW', 'as89lokan07')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
